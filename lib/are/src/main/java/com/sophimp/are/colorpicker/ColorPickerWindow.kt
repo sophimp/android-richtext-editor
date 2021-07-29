8085899000b90fb93ee64f1cf438e73db361d28e
Sophimp <songfangxi2012@163.com>
2021-07-29 21:13:13 +0800

    [feature] color pick window demo

---
+++ lib/are/src/main/java/com/sophimp/are/colorpicker/ColorPickerWindow.kt
@@ -0,0 +1,74 @@
+package com.sophimp.are.colorpicker
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.PopupWindow
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sophimp.are.R
+import com.sophimp.are.databinding.PopupWindowColorBinding
+
+/**
+ *
+ * @author: sfx
+ * @since: 2021/7/29
+ */
+class ColorPickerWindow(context: Context) : PopupWindow(context) {
+    lateinit var colors: IntArray
+
+    var colorPickerListener: ColorPickerListener? = null
+
+    init {
+        initView(context)
+    }
+
+    private fun initView(context: Context) {
+        colors = context.resources.getIntArray(R.array.colorPickerColors)
+        val binding = PopupWindowColorBinding.inflate(LayoutInflater.from(context))
+        contentView = binding.root
+        binding.rvPalette.layoutManager = GridLayoutManager(context, 2, GridLayoutManager.HORIZONTAL, false)
+        binding.rvPalette.adapter = ColorAdapter()
+    }
+
+    inner class ColorAdapter : RecyclerView.Adapter<ColorAdapter.ColorHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorHolder {
+            val layoutParams = RecyclerView.LayoutParams((parent.context.resources.displayMetrics.density * 30).toInt(),
+                (parent.context.resources.displayMetrics.density * 30).toInt())
+            val view = TextView(parent.context)
+            view.layoutParams = layoutParams
+            layoutParams.leftMargin = (parent.context.resources.displayMetrics.density * 5).toInt()
+            layoutParams.rightMargin = (parent.context.resources.displayMetrics.density * 5).toInt()
+            layoutParams.topMargin = (parent.context.resources.displayMetrics.density * 5).toInt()
+            layoutParams.bottomMargin = (parent.context.resources.displayMetrics.density * 5).toInt()
+            return ColorHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return this@ColorPickerWindow.colors.size
+        }
+
+        override fun onBindViewHolder(holder: ColorHolder, position: Int) {
+            val bg = GradientDrawable()
+            bg.cornerRadius = holder.itemView.context.resources.displayMetrics.density * 40
+//            Util.log("colors size: ${this@ColorPickerWindow.colors.size} ${this@ColorPickerWindow.colors.contentToString()}")
+            if (this@ColorPickerWindow.colors.isNotEmpty()) {
+                val color = this@ColorPickerWindow.colors[position]
+                bg.colors = intArrayOf(color, color)
+            }
+            holder.itemView.background = bg
+        }
+
+        inner class ColorHolder(view: View) : RecyclerView.ViewHolder(view) {
+            init {
+                view.setOnClickListener {
+                    this@ColorPickerWindow.colorPickerListener?.onPickColor(this@ColorPickerWindow.colors[adapterPosition])
+                }
+            }
+        }
+    }
+}
--- lib/are/src/main/java/com/sophimp/are/toolbar/items/FontColorToolItem.kt
+++ lib/are/src/main/java/com/sophimp/are/toolbar/items/FontColorToolItem.kt
@@ -1,10 +1,30 @@
 package com.sophimp.are.toolbar.items
 
+import android.view.Gravity
 import com.sophimp.are.R
+import com.sophimp.are.colorpicker.ColorPickerListener
+import com.sophimp.are.colorpicker.ColorPickerWindow
 import com.sophimp.are.style.FontColorStyle
 
 class FontColorToolItem(style: FontColorStyle) :
     AbstractItem(style) {
+    private val colorWindow = ColorPickerWindow(context)
+
+    init {
+        colorWindow.colorPickerListener = object : ColorPickerListener {
+            override fun onPickColor(color: Int) {
+                iconView.setBackgroundColor(color)
+                style.onPickColor(color)
+                colorWindow.dismiss()
+            }
+        }
+    }
+
+    override fun iconClickHandle() {
+        super.iconClickHandle()
+        colorWindow.showAsDropDown(iconView, -(iconView.x).toInt(), -(context.resources.displayMetrics.density * 80).toInt(), Gravity.TOP)
+    }
+
     override val srcResId: Int
         get() = R.mipmap.icon_toolitem_foregroundcolor
 
